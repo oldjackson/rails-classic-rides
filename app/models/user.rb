5c9cdb905a4c18cb6f61c283d8239000ebad96cb
Lorenzo Fermi <lorenzofermi@gmail.com>
2018-02-26 18:40:00 +0000

    User: ownership of cars and bookings, validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  has_many :cars, dependent: :destroy
+  has_many :bookings
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
