80c8a2a66b4b453dd0e7e83eb8342f764bb3336e
Lorenzo Fermi <Lorenzofermi@gmail.com>
2018-02-27 11:37:26 +0000

    Bookings: schema and validation

---
+++ app/models/booking.rb
@@ -0,0 +1,26 @@
+class Booking < ApplicationRecord
+  belongs_to :user
+  belongs_to :car
+
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validate :start_date_cannot_be_in_the_past,
+    :end_date_cannot_be_before_start_date
+  validates :status, inclusion: {in: %w(pending, accepted, declined, ongoing, completed)}
+
+  validates :total_price, presence: true, numericality: { greater_than: 0}
+
+  private
+
+  def start_date_cannot_be_in_the_past
+    if start_date < Date.today
+      errors.add(:start_date, "can't be in the past")
+    end
+  end
+
+  def end_date_cannot_be_before_start_date
+    if end_date < start_date
+      errors.add(:end_date, "can't be before start date")
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180226191741) do
+ActiveRecord::Schema.define(version: 20180227105946) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "car_id"
+    t.date "start_date"
+    t.date "end_date"
+    t.float "total_price"
+    t.string "status", default: "pending"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["car_id"], name: "index_bookings_on_car_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "cars", force: :cascade do |t|
     t.string "make"
@@ -52,5 +65,7 @@ ActiveRecord::Schema.define(version: 20180226191741) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "cars"
+  add_foreign_key "bookings", "users"
   add_foreign_key "cars", "users"
 end
