62060c01b95b052dcc6fb869a85ea0c636c73625
Lorenzo <lorenzofermi@gmail.com>
2018-02-28 13:49:27 +0000

    Remove now useless booking routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
   resources :cars do
-    resources :bookings, only: [:new, :show, :create, :edit]
+    resources :bookings, only: [:new, :create]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
