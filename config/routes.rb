cedd8f9b336e9988e3c9f787df948e7889c1b929
Marta Keller <marta.keller.pie@gmail.com>
2018-02-28 13:22:52 +0000

    form bookings

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
       }
 
   root to: 'pages#home'
-  resources :cars
+  resources :cars do
+    resources :bookings, only: [:new, :show, :create, :edit]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
