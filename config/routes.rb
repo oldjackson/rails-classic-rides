419dc6a848bdca080b9805da54ab35dff1299a7e
Lorenzo Fermi <Lorenzofermi@gmail.com>
2018-03-01 12:35:24 +0000

    Route for user#show added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/show'
+
   devise_for :users, controllers: {
         registrations: 'users/registrations'
       }
@@ -7,5 +9,7 @@ Rails.application.routes.draw do
   resources :cars do
     resources :bookings, only: [:new, :create]
   end
+
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
