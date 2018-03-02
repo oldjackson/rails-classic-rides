3823a673d3c79c6225ccb0cf0e3009917aaed836
Carly Petracco <carly.k.p@gmail.com>
2018-03-02 12:44:09 +0000

    deleting extra photo files

--- app/javascript/packs/maps.js
+++ app/javascript/packs/maps.js
@@ -114,7 +114,6 @@ const styles = [
     }
 ]
 
-
 const mapElement = document.getElementById('map');
 if (mapElement) { // don't try to build a map if there's no div#map to inject in
   const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
